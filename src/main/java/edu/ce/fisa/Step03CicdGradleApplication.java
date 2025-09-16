e5022a6bffbb3ed927dbf048b3daba8ec5abe384
jihwan77 <hjw780365@gmail.com>
2025-09-16 10:40:32 +0900

    first

---
+++ src/main/java/edu/ce/fisa/Step03CicdGradleApplication.java
@@ -0,0 +1,13 @@
+package edu.ce.fisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step03CicdGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step03CicdGradleApplication.class, args);
+	}
+
+}
